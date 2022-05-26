b5355e42fe4168fd1a50de2f4c3e1f2b8a5c363e
piedra <piedra@cern.ch>
2022-05-26 19:42:20 +0200

    Minor changes

--- compile.sh
+++ compile.sh
@@ -7,6 +7,7 @@ if [ $# -lt 1 ]; then
     echo "  "
     echo " ./compile.sh FBIII-Examen-2022.tex"
     echo " ./compile.sh FBIII-Problemas-2022.tex"
+    echo " ./compile.sh FBIII-Practica-6.tex"
     echo "  "
     exit -1
 fi
