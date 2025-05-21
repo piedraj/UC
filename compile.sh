ec3a24571f6c279257bf86926668b9279da953e1
piedraj <piedra@cern.ch>
2025-05-21 09:10:15 +0200

    Add files via upload

--- compile.sh
+++ compile.sh
@@ -4,10 +4,11 @@ if [ $# -lt 1 ]; then
     echo "  "
     echo " ./compile.sh LFI-Aerodinamica.tex"
     echo " ./compile.sh LFI-Pohl.tex"
+    echo " ./compile.sh LFI-Examen.tex"
     echo "  "
-    echo " ./compile.sh FBIII-Examen.tex"
     echo " ./compile.sh FBIII-Problemas.tex"
     echo " ./compile.sh FBIII-Practica-6.tex"
+    echo " ./compile.sh FBIII-Examen.tex"
     echo "  "
     exit -1
 fi
